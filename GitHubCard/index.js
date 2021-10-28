13fe1cfbc40b5904f282f5fc9530890d05c45721
Claire List <claire.list2501@gmail.com>
2021-10-28 13:53:39 -0600

    axios get working

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,6 +3,14 @@
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+import axios from 'axios';
+
+axios.get('https://api.github.com/users/clairelist')
+.then(res=>{
+  console.log(res);
+}).catch(err=>{
+  console.error(err);
+})
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
